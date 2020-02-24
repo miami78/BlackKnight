4a4067072d78b0b890d210f45afb92c799dd6255
miami78 <miamilarry78@gmail.com>
2020-02-24 12:25:32 +0300

    place player1 at random

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,4 @@
 // game Setup
-// players
-class Player {
-    constructor(name,weapon,health,position)	{
-		this.playerName = name,
-		this.playerWeapon = weapon,
-		this.health = health,
-		this.playerPos = position
-	}
-} 
-
-let knight = new Player ("knight","<img src='assets/img/player1.png' id='knight' class='player1' alt='Knight'>",100, {col:0,row:0})
-let dragon = new Player ("dragon","<img src='../img/player2.png' id='dragon' class='player2' alt='Dragon'>",100, {col:8,row:8})
-
-
-// weapons
-
-class Weapon {
-	constructor(name,damage,image) {
-		this.name = name;
-		this.damage = damage;
-		this.image = image;
-	}
-}
-let sword = new Weapon ("sword","<img src='../img/weapon1.png' id='sword' class='weapon' alt='Sword");
-// let fire = new Weapon ("fire","<img src='../img/weapon2.png' id='fire' class='weapon' alt='Fire");
-
-// obstacles
-
-let wall = "";
-let tree = "";
-
-// Random number function
-function randomNum() {
-	return Math.floor(Math.random() * (11 - 1) + 1);
-}
-
 
 // Creates the grid
 function createGrid() {
@@ -45,30 +9,77 @@ function createGrid() {
 	}
 }
 
-// places players randomly 
-function placePlayers(player) {
-	let coordinates = {
-		row: this.randomNum(),
-		column: this.randomNum()
-	};
-	$(`[data-row="${coordinates.row}"][data-column="${coordinates.column}"]`).addClass(player["name"]);
-	player["position"].row = coordinates.row;
-	player["position"].column = coordinates.column;
-}
-
-// create barriers at random squares
-function createBarrier() {
-	let coordinates = {
-		row: this.randomNum(),
-		column: this.randomNum()
-	};
+// Random number function
+function randomNum() {
+	return Math.floor(Math.random() * (11 - 1) + 1);
 }
 
 function placeImg (image, position){
 	// take position 
 	// use line 52 as reference, what is position 'data type'
 	$(`[data-row="${position.row}"][data-column="${position.column}"]`).append(image);
+	// $(`[data-row="1"][data-column="1"]`).append(knightImg);
+	console.log($(`[data-row="1"][data-column="1"]`))
 }
 
-placeImg(knight.playerWeapon, knight.playerPos);
+// places players randomly 
+function placePlayers(player) {
+	let coordinates = {
+		row: this.randomNum(),
+		column: this.randomNum()
+	};
+	player.playerPos = coordinates;
+	placeImg(player.playerImage, player.playerPos);
+}
+
+// create barriers at random squares
+function createBarrier(barrier) {
+	let coordinates = {
+		row: this.randomNum(),
+		column: this.randomNum()
+	};
+}
+
+// players
+class Player {
+    constructor(name,image,health,position)	{
+		this.playerName = name,
+		this.playerImage = image,
+		this.health = health,
+		this.playerPos = position
+	}
+} 
+//knight
+let knightImg = document.createElement("img");
+knightImg.src = 'assets/img/player1.png';
+knightImg.id = 'knight';
+knightImg.className = 'player1';
+//dragon
+let dragonImg = document.createElement("img");
+dragonImg.src = 'assets/img/player2.png';
+dragonImg.id = 'dragon';
+dragonImg.className = 'player2';
+
+let knight = new Player ("knight",knightImg,100, {column:1,row:1})
+let dragon = new Player ("dragon",dragonImg,100, {column:8,row:8})
+
+
+// weapons
+class Weapon {
+	constructor(name,damage,image) {
+		this.name = name;
+		this.damage = damage;
+		this.image = image;
+	}
+}
+let sword = new Weapon ("sword","<img src='../img/weapon1.png' id='sword' class='weapon' alt='Sword");
+// let fire = new Weapon ("fire","<img src='../img/weapon2.png' id='fire' class='weapon' alt='Fire");
+
+// barriers
+
+let wall = "";
+let tree = "";
+
 createGrid();
+placePlayers(knight)
+
